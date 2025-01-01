d3394f4042e4048f77984728d49c3df3086efc31
Maxi Gorynski <supriyarai@Supriyas-MacBook-Pro.local>
2025-01-01 18:38:13 +0000

    Tokeniser V1

--- Tokeniser.py
+++ Tokeniser.py
@@ -2,10 +2,60 @@ import re
 
 with open ("the-verdict.txt.rtf", "r", encoding="utf-8") as f:
     raw_text = f.read()
-print ("Total number of characters:", len(raw_text))
-print (raw_text[:99])
+#print ("Total number of characters:", len(raw_text))
+#print (raw_text[:99])
 
 #Simple tokenisation
-text = "Hello world. Check out this test."
-result = re.split(r'(\s)', text)
-print(result)
+text = "Hello world. Is this-- a test?"
+result = re.split(r'([,.:;?_!"()\']|--|\s)', text)
+result = [item.strip() for item in result if item.strip()]
+#print(result)
+
+preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
+preprocessed = [item.strip() for item in preprocessed if item.strip()]
+#print(len(preprocessed))
+#print(preprocessed[:30])
+
+all_words = sorted(set(preprocessed))
+vocab_size = len(all_words)
+#print(vocab_size)
+
+vocab = {token:integer for integer, token in enumerate(all_words)}
+for i, item in enumerate(vocab.items()):
+    #print(item)
+    if i >= 50:
+        break
+
+import re
+
+
+class SimpleTokeniserV1:
+    def __init__(self, vocab):
+        # Stores the vocab as a class attribute for access in the encode and decode methods
+        self.str_to_int = vocab
+        # Creates an inverse vocabulary that maps token IDs back to tokens
+        self.int_to_str = {i: s for s,i in vocab.items()}
+
+    # Processes input text into token ids
+    def encode(self, text):
+        # Updated regex for correct tokenization
+        preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', text)
+        preprocessed = [item.strip() for item in preprocessed if item.strip()]
+        ids = [self.str_to_int[s] for s in preprocessed]
+        return ids
+
+    def decode(self, ids):
+        text = " ".join([self.int_to_str[i] for i in ids])
+        # Removes spaces before specified punctuation
+        text = re.sub(r'\s+([,.?!"()\'])',r'\1', text)
+        return text
+
+
+tokeniser = SimpleTokeniserV1(vocab)
+text = """It's the last he painted, you know," Mrs. Gisburn said with pardonable pride."""
+ids = tokeniser.encode(text)
+print(ids)
+print(tokeniser.decode(ids))
+
+text = "Hello, would you like a cuppa?"
+print(tokeniser.encode(text))
