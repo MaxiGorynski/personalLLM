91e793ac0cb767dc39d1d7491a0fb0a2324c919d
Maxi Gorynski <supriyarai@Supriyas-MacBook-Pro.local>
2025-01-03 17:33:08 +0000

    Causal attention, masking, weights calculation, next step context vector calculations

--- LLM.py
+++ LLM.py
@@ -359,4 +359,57 @@ class SelfAttention_v1(nn.Module):
 
 torch.manual_seed(123)
 sa_v1 = SelfAttention_v1(d_in, d_out)
-print(sa_v1(inputs))
+#print(sa_v1(inputs))
+
+class SelfAttention_v2(nn.Module):
+    def __init__(self, d_in, d_out, qkv_bias=False):
+        super().__init__()
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+
+    def forward(self, x):
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+        attn_scores = queries @ keys.T
+        attn_weights = torch.softmax(attn_scores/ keys.shape[-1] ** 0.5, dim=-1)
+        context_vec = attn_weights @ values
+        return context_vec
+
+torch.manual_seed(789)
+sa_v2 = SelfAttention_v2(d_in, d_out)
+#print(sa_v2(inputs))
+
+#Compute attention weights for masked attention
+queries = sa_v2.W_query(inputs)
+keys = sa_v2.W_key(inputs)
+attn_scores = queries @ keys.T
+attn_weights = torch.softmax(attn_scores/keys.shape[-1] ** 0.5, dim=-1)
+#print(attn_weights)
+
+#Use tril to create a mask where values above diagonal are zero
+context_length = attn_scores.shape[0]
+mask_simple = torch.tril(torch.ones(context_length, context_length))
+#print(mask_simple)
+
+#Multiply the mask with the attention weights to zero-out results above the diagonal
+masked_simple = attn_weights*mask_simple
+#print(masked_simple)
+
+#Renormalise the attention weights to sum up to one
+row_sums = masked_simple.sum(dim=-1, keepdim=True)
+masked_simple_norm = masked_simple/row_sums
+#print(masked_simple_norm)
+
+#Make the thing more efficient by replacing zeros above the diagonal with negatinve infinit
+mask = torch.triu(torch.ones(context_length, context_length), diagonal=1)
+masked = attn_scores.masked_fill(mask.bool(), -torch.inf)
+#print(masked)
+
+#Apply softmax to acquire the complete modified attention weights
+attn_weights = torch.softmax(masked/keys.shape[-1] ** 0.5, dim=1)
+print(attn_weights)
+
+
+
