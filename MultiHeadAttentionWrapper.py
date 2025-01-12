73287d56bf4569a37facebde0fe29fa3fdad36ad
Maxi Gorynski <supriyarai@Supriyas-MacBook-Pro.local>
2025-01-12 20:44:44 +0000

    MultiHeadAttention module

---
+++ MultiHeadAttentionWrapper.py
@@ -0,0 +1,23 @@
+import re
+from importlib.metadata import version
+import tiktoken
+import torch
+from torch.utils.data import Dataset, DataLoader
+import torch.nn as nn
+
+class MultiHeadAttentionWrapper(nn.Module):
+    def __init__(self, d_in, d_out, context_length, dropout, num_heads, qkv_bias=False):
+        super().__init__()
+        self.heads = nn.ModuleList(
+            [CausalAttention(
+                d_in, d_out, context_length, dropout, qkv_bias
+            )
+                for _ in range(num_heads)]
+        )
+
+    def forward(self, x):
+        return torch.cat([head(x) for head in self.heads], dim=-1)
+
+
+torch.manual_seed(123)
+context_length = batch.shape[1] #Number of tokens
