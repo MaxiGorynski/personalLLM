f328a5b6c7e51189e72912d44c00f3ab3fd4b6b4
Maxi Gorynski <supriyarai@Supriyas-MacBook-Pro.local>
2025-02-20 18:55:55 +0000

    Early training epochs completed

--- LLM.py
+++ LLM.py
@@ -1107,8 +1107,77 @@ model.to(device)
 with torch.no_grad():
     train_loss = calc_loss_loader(train_loader, model, device)
     val_loss = calc_loss_loader(val_loader, model, device)
-print ("Training loss:", train_loss)
-print ("Validation loss:", val_loss)
+#print ("Training loss:", train_loss)
+#print ("Validation loss:", val_loss)
+
+def evaluate_model(model, train_loader, val_loader, device, eval_iter):
+    model.eval()
+    with torch.no_grad():
+        train_loss = calc_loss_loader(train_loader, model, device, num_batches=eval_iter)
+        val_loss = calc_loss_loader(val_loader, model, device, num_batches=eval_iter)
+    model.train()
+    return train_loss, val_loss
 
 
+def generate_and_print_sample(model, tokeniser, device, start_context):
+    model.eval()
+    context_size = model.pos_emb.weight.shape[0]
+    encoded = text_to_token_ids(start_context, tokeniser).to(device)
+    with torch.no_grad():
+        token_ids = generate_text_simple(model = model, idx=encoded, max_new_tokens=50, context_size=context_size)
+    decoded_text = token_ids_to_text(token_ids, tokeniser)
+    print(decoded_text.replace("\n", " "))
+    model.train()
+def train_model_simple(model, train_loader, val_loader, optimiser, device, num_epochs, eval_freq, eval_iter, start_context, tokeniser):
+    train_losses, val_losses, track_tokens_seen = [], [], [] #Inintialise lists to track losses and tokens seen
+    tokens_seen, global_step = 0, -1
+
+    for epoch in range(num_epochs): #Starts main training loop
+        model.train()
+        for input_batch, target_batch in train_loader:
+            optimiser.zero_grad() #Resets loss gradients from previous batch iteration
+            loss = calc_loss_batch(input_batch, target_batch, model, device)
+            loss.backward() #Calculates loss gradients
+            optimiser.step() #Updates model weights using loss gradients
+            tokens_seen += input_batch.numel()
+            global_step += 1
+
+            if global_step % eval_freq == 0: # Optional eval step
+                train_loss, val_loss = evaluate_model(model, train_loader, val_loader, device, eval_iter)
+                train_losses.append(train_loss)
+                val_losses.append(val_loss)
+                track_tokens_seen.append(tokens_seen)
+                print(f"Ep {epoch+1} (Step {global_step:06d}): "
+                      f"Train loss {train_loss:.3f}, "
+                      f"Val loss {val_loss:.3f}")
+
+        generate_and_print_sample(model, tokeniser, device, start_context)
+    return train_losses, val_losses, track_tokens_seen
+
+
+torch.manual_seed(123)
+model = GPTModel(GPT_CONFIG_124M)
+model.to(device)
+optimiser = torch.optim.AdamW(model.parameters(), lr=0.0004, weight_decay=0.1)
+num_epochs = 15
+train_losses, val_losses, tokens_seen = train_model_simple(model, train_loader, val_loader, optimiser, device, num_epochs=num_epochs, eval_freq=5, eval_iter=5, start_context="Every effort moves you", tokeniser=tokeniser)
+
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+def plot_losses(epochs_seen, tokens_seen, train_losses, val_losses):
+    fig, ax1 = plt.subplots(figsize=(5, 3))
+    ax1.plot(epochs_seen, train_losses, label="Training loss")
+    ax1.plot(epochs_seen, val_losses, linestyle="-.", label="Validation loss")
+    ax1.set_xlabel("Epochs")
+    ax1.set_ylabel("Loss")
+    ax1.legend(loc="upper right")
+    ax1.xaxis.set_major_locator(MaxNLocator(integer=True))
+    ax2 = ax1.twiny()
+    ax2.plot(tokens_seen, train_losses, alpha=0)
+    ax2.set_xlabel("Tokens seen")
+    fig.tight_layout()
+    plt.show()
+
+epochs_tensor = torch.linspace(0, num_epochs, len(train_losses))
+plot_losses(epochs_tensor, tokens_seen, train_losses, val_losses)
 
