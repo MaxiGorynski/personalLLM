5966faa389e9fef30833cf4c63297c7c1a982380
Maxi Gorynski <supriyarai@Supriyas-MacBook-Pro.local>
2025-02-20 17:24:49 +0000

    Loss loader, training and validation set creation

--- LLM.py
+++ LLM.py
@@ -801,8 +801,8 @@ def print_gradients(model, x):
 
     loss.backward()
 
-    for name, param in model.named_parameters():
-        if 'weight' in name:
+    #for name, param in model.named_parameters():
+        #if 'weight' in name:
             #print(f"{name} has gradient mean of {param.grad.abs().mean().item()}")
 
 
@@ -849,17 +849,17 @@ output = block(x)
 #print("Input shape:", x.shape)
 #print("Output shape:", output.shape)
 
-class GPTModel(nn.module):
+class GPTModel(nn.Module):
     def __init__(self, cfg):
         super().__init__()
         self.tok_emb = nn.Embedding(cfg["vocab_size"], cfg["emb_dim"])
         self.pos_emb = nn.Embedding(cfg["context_length"], cfg["emb_dim"])
-        self.drop_emb == nn.Dropout(cfg["drop_rate"])
+        self.drop_emb = nn.Dropout(cfg["drop_rate"])
 
         self.trf_blocks = nn.Sequential( * [TransformerBlock(cfg) for _ in range (cfg["n_layers"])])
 
         self.final_norm = LayerNorm(cfg["emb_dim"])
-        self.out_head = nn.Linear(cfg["emb_dim"]. cfg["vocab_size"], bias=False)
+        self.out_head = nn.Linear(cfg["emb_dim"], cfg["vocab_size"], bias=False)
 
     def forward(self, in_idx):
         batch_size, seq_len = in_idx.shape
@@ -872,4 +872,243 @@ class GPTModel(nn.module):
         x = self.final_norm(x)
         logits = self.out_head(x)
         return logits
+
+torch.manual_seed(123)
+model = GPTModel(GPT_CONFIG_124M)
+
+out = model(batch)
+#print("Input batch:\n", batch)
+#print("\nOutput shape:", out.shape)
+#print(out)
+
+total_params = sum(p.numel() for p in model.parameters())
+#print(f"Total number of parameters: {total_params:,}")
+
+#print("Token embedding layer shape:", model.tok_emb.weight.shape)
+#print("Output layer shape:", model.out_head.weight.shape)
+
+total_params_gpt2 = (
+    total_params - sum(p.numel()
+    for p in model.out_head.parameters())
+)
+#print(f"Number of trainable parameters "
+      #f"considering weight tying: {total_params_gpt2:,}")
+
+total_size_bytes = total_params * 4
+total_size_mb = total_size_bytes / (1024 * 1024)
+#print(f"Total size of the model: {total_size_mb:.2f} MB")
+
+def get_config(base_config, model_name="gpt2-small"):
+    GPT_CONFIG = base_config.copy()
+
+    if model_name == "gpt2-small":
+        GPT_CONFIG["emb_dim"] = 768
+        GPT_CONFIG["n_layers"] = 12
+        GPT_CONFIG["n_heads"] = 12
+
+    elif model_name == "gpt2-medium":
+        GPT_CONFIG["emb_dim"] = 1024
+        GPT_CONFIG["n_layers"] = 24
+        GPT_CONFIG["n_heads"] = 16
+
+    elif model_name == "gpt2-large":
+        GPT_CONFIG["emb_dim"] = 1280
+        GPT_CONFIG["n_layers"] = 36
+        GPT_CONFIG["n_heads"] = 20
+
+    elif model_name == "gpt2-xl":
+        GPT_CONFIG["emb_dim"] = 1600
+        GPT_CONFIG["n_layers"] = 48
+        GPT_CONFIG["n_heads"] = 25
+
+    else:
+        raise ValueError(f"Incorrect model name {model_name}")
+
+    return GPT_CONFIG
+
+def generate_text_simple(model, idx, max_new_tokens, context_size):
+    for _ in range(max_new_tokens):
+        idx_cond = idx[:, -context_size:]
+        with torch.no_grad():
+            logits = model(idx_cond)
+
+        logits = logits[:, -1, :]
+        probas = torch.softmax(logits, dim=-1)
+        idx_next = torch.argmax(probas, dim=-1, keepdim=True)
+        idx = torch.cat((idx, idx_next), dim=1)
+
+    return idx
+
+start_context = "Hello, I am"
+encoded = tokeniser.encode(start_context)
+#print("encoded: ", encoded)
+encoded_tensor = torch.tensor(encoded).unsqueeze(0)
+#print("encoded_tensor.shape", encoded_tensor.shape)
+
+model.eval()
+out = generate_text_simple(
+    model=model,
+    idx=encoded_tensor,
+    max_new_tokens=6,
+    context_size=GPT_CONFIG_124M["context_length"]
+)
+#print("Output:", out)
+#print("Output length:", len(out[0]))
+
+decoded_text = tokeniser.decode(out.squeeze(0).tolist())
+#print(decoded_text)
+
+GPT_CONFIG_124M = {
+    "vocab_size": 50257,
+    "context_length": 256,
+    "emb_dim": 768,
+    "n_heads": 12,
+    "n_layers": 12,
+    "drop_rate": 0.1,
+    "qkv_bias": False
+}
+torch.manual_seed(123)
+model = GPTModel(GPT_CONFIG_124M)
+model.eval()
+
+import tiktoken
+def text_to_token_ids(text, tokeniser):
+    encoded = tokeniser.encode(text, allowed_special={'<|endoftext|>'})
+    encoded_tensor = torch.tensor(encoded).unsqueeze(0) #Adds the batch dimension
+    return encoded_tensor
+
+def token_ids_to_text(token_ids, tokeniser):
+    flat = token_ids.squeeze(0) #Removes batch dimension
+    return tokeniser.decode(flat.tolist())
+
+start_context = "Every effort moves you"
+tokeniser = tiktoken.get_encoding("gpt2")
+
+token_ids = generate_text_simple(
+    model=model,
+    idx=text_to_token_ids(start_context, tokeniser),
+    max_new_tokens=10,
+    context_size=GPT_CONFIG_124M["context_length"]
+)
+#print ("Output text:\n", token_ids_to_text(token_ids, tokeniser))]]
+
+inputs = torch.tensor([[16833, 3626, 6100], [40, 1107, 588]]) #"Every effort moves", and "I really like"
+
+targets = torch.tensor([[3626, 6100, 345], [1107, 588, 11311]]) #"effort moves you", and "really like chocolate"
+
+with torch.no_grad(): #Disables gradient tracking, we are not training yet
+    logits = model(inputs)
+probas = torch.softmax(logits, dim=-1) #Probability of each token in vocabulary
+#print(probas.shape)
+
+token_ids = torch.argmax(probas, dim=-1, keepdim=True)
+#print("Token IDs:\n", token_ids)
+
+#print(f"Targets batch 1: {token_ids_to_text(targets[0], tokeniser)}")
+#print(f"Outputs batch 1:" f"{token_ids_to_text(token_ids[0].flatten(), tokeniser)}")
+
+text_idx = 0
+target_probas_1 = probas[text_idx, [0,1,2], targets[text_idx]]
+#print("Text 1:", target_probas_1)
+
+text_idx = 1
+target_probas_2 = probas[text_idx, [0,1,2], targets[text_idx]]
+#print("Text 2:", target_probas_2)
+
+log_probas = torch.log(torch.cat((target_probas_1, target_probas_2)))
+#print(log_probas)
+
+avg_log_probas = torch.mean(log_probas)
+#print(avg_log_probas)
+
+neg_avg_log_probas = avg_log_probas * -1
+#print(neg_avg_log_probas)
+
+#print("Logits shape:", logits.shape)
+#print("Targets shape:", targets.shape)
+
+logits_flat = logits.flatten(0, 1)
+targets_flat = targets.flatten()
+#print("Flattened logits:", logits_flat.shape)
+#print("Flattened targets:", targets_flat.shape)
+
+loss = torch.nn.functional.cross_entropy(logits_flat, targets_flat)
+#print(loss)
+
+file_path = "the-verdict.txt"
+with open(file_path, "r", encoding="utf-8") as file:
+    text_data = file.read()
+
+total_characters = len(text_data)
+total_tokens = len(tokeniser.encode(text_data))
+#print("Characters:", total_characters)
+#print("Tokens:", total_tokens)
+
+train_ratio = 0.90
+split_idx = int(train_ratio * len(text_data))
+train_data = text_data[:split_idx]
+val_data = text_data[split_idx:]
+
+torch.manual_seed(123)
+
+train_loader = create_dataloader_v1(
+    train_data,
+    batch_size=2,
+    max_length=GPT_CONFIG_124M["context_length"],
+    stride=GPT_CONFIG_124M["context_length"],
+    drop_last=True,
+    shuffle=True,
+    num_workers=0
+)
+
+val_loader = create_dataloader_v1(
+    val_data,
+    batch_size=2,
+    max_length=GPT_CONFIG_124M["context_length"],
+    stride=GPT_CONFIG_124M["context_length"],
+    drop_last=False,
+    shuffle=False,
+    num_workers=0
+)
+
+#print("Train loader:")
+#for x, y in train_loader:
+    #print(x.shape, y.shape)
+
+#print("\nValidation loader:")
+#for x, y in val_loader:
+    #print(x.shape, y.shape)
+
+def calc_loss_batch(input_batch, target_batch, model, device):
+    input_batch = input_batch.to(device)
+    target_batch = target_batch.to(device)
+    logits = model(input_batch)
+    loss = torch.nn.functional.cross_entropy(logits.flatten(0,1), target_batch.flatten())
+    return loss
+
+def calc_loss_loader(data_loader, model, device, num_batches=None):
+    total_loss = 0.
+    if len(data_loader) == 0:
+        return float("nan")
+    elif num_batches is None:
+        num_batches = len(data_loader) #Iterates over all batches if no fixed num_batches is specified
+    else:
+        num_batches = min(num_batches, len(data_loader)) #Reduces the number of batches to match number of batches in data loader is num_batches exceeds number of batches in the data loader
+    for i, (input_batch, target_batch) in enumerate(data_loader):
+        if i < num_batches:
+            loss = calc_loss_batch(input_batch, target_batch, model, device)
+            total_loss += loss.item() #Sums loss for each batch
+        else:
+            break
+    return total_loss/num_batches #Averages the loss over all batches
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+with torch.no_grad():
+    train_loss = calc_loss_loader(train_loader, model, device)
+    val_loss = calc_loss_loader(val_loader, model, device)
+print ("Training loss:", train_loss)
+print ("Validation loss:", val_loss)
+
+
 
