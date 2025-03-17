b26fd744fd3611b0d90d929ae0e21201031535ff
Maxi Gorynski <supriyarai@Supriyas-MacBook-Pro.local>
2025-03-17 13:22:56 +0000

    Finetuning, dataset download, balancing, training/eval subset split

--- LLM.py
+++ LLM.py
@@ -1453,7 +1453,83 @@ token_ids = generate(
     top_k = 50,
     temperature = 1.5
 )
+
+'''
 print("Output text :\n", token_ids_to_text(token_ids, tokeniser))
 train_loss = calc_loss_loader(train_loader, gpt, device)
 val_loss = calc_loss_loader(val_loader, gpt, device)
 print(train_loss, val_loss)
+'''
+
+import urllib.request
+import zipfile
+import os
+from pathlib import Path
+
+url = "https://archive.ics.uci.edu/static/public/228/sms+spam+collection.zip"
+zip_path = "sms_spam_collection.zip"
+extracted_path = "sms_spam_collection"
+data_file_path = Path(extracted_path) / "SMSSpamCollection.tsv"
+
+def download_and_unzip_spam_data(
+        url, zip_path, extracted_path, data_file_path):
+    if data_file_path.exists():
+        print(f"{data_file_path} already exists. Skipping download "
+              "and extraction."
+        )
+        return
+
+    with urllib.request.urlopen(url) as response:
+        with open(zip_path, "wb") as out_file:
+            out_file.write(response.read())
+
+    with zipfile.ZipFile(zip_path, "r") as zip_ref:
+        zip_ref.extractall(extracted_path)
+
+    original_file_path = Path(extracted_path) / "SMSSpamCollection"
+    os.rename(original_file_path, data_file_path)
+    print(f"File downloaded and saved as {data_file_path}")
+
+#download_and_unzip_spam_data(url, zip_path, extracted_path, data_file_path)
+
+import pandas as pd
+df = pd.read_csv(data_file_path, sep="\t", header=None, names=["Label", "Text"])
+#print(df)
+
+#print(df["Label"].value_counts())
+
+def create_balanced_dataset(df):
+    #Count the number of "spam" samples in the dataset
+    num_spam = df[df["Label"] == "spam"].shape[0]
+
+    # Randomly sample an equal number of "ham" (non-spam) messages to balance the dataset. Fixing random_state for reproducibility.
+    ham_subset = df[df["Label"] == "ham"].sample(num_spam, random_state=123)
+
+    #Concatenate the sampled "ham" messages with all "spam" messages to create a balanced DataFrame.
+    balanced_df = pd.concat([ham_subset, df[df["Label"] == "spam"]])
+
+    #Return the balanced dataset
+    return balanced_df
+
+balanced_df = create_balanced_dataset(df)
+#print(balanced_df["Label"].value_counts())
+
+balanced_df["Label"] = balanced_df["Label"].map({"ham": 0, "spam": 1})
+
+def random_split(df, train_frac, validation_frac):
+
+    df = df.sample(frac=1, random_state=123).reset_index(drop=True)
+    train_end = int(len(df) * train_frac)
+    validation_end = train_end + int(len(df) * validation_frac)
+
+    train_df = df[:train_end]
+    validation_df = df[train_end:validation_end]
+    test_df = df[validation_end:]
+
+    return train_df, validation_df, test_df
+
+train_df, validation_df, test_df = random_split(balanced_df, 0.7, 0.1)
+
+train_df.to_csv("train.csv", index=None)
+validation_df.to_csv("validation.csv", index=None)
+test_df.to_csv("test_csv", index=None)
